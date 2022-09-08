ecaee0d2c6ae503a604a326cc99b4ecaac2f9814
Alex Martyan <alexanderthemartian@gmail.com>
2022-09-08 15:45:28 +0300

    feat: add calendar test task for Genesis Holy Water

---
+++ holy/src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.module.scss';
+import CalendarContainer from "./components/CalendarContainer/CalendarContainer";
+
+
+const App = () => {
+  return (
+    <div className={styles.wrapper}>
+      <CalendarContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ holy/src/store/reducers/eventsReducer.js
@@ -0,0 +1,27 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {getFromLS} from "../../functions/localStorage";
+
+const initialState = {
+  events: getFromLS("events") ? getFromLS("events") : [],
+};
+
+const eventSlice = createSlice({
+  name: 'events',
+  initialState,
+  reducers: {
+    addEvent(state, payload) {
+      state.events.push(payload.payload);
+    },
+    editEvent(state, payload) {
+      const index = state.events.findIndex(item => item.id === payload.payload.id);
+      state.events[index] = payload.payload;
+    },
+    deleteEvent(state, payload) {
+      const index = state.events.findIndex(item => item.id === payload.payload);
+      state.events.splice(index);
+    }
+  }
+})
+
+export default eventSlice.reducer;
+export const {addEvent, editEvent, deleteEvent} = eventSlice.actions;
